2ca315fe5d9f7dfa4290bdef570c8455c1e37884
BachaBajceps <dla50@interia.pl>
2024-05-25 20:32:21 +0200

    Revert "Merge branch 'master' into serwer"
    This reverts commit 31030cf08d30b23c12ce1eb65cd03d97ac4b9236, reversing
    changes made to caf88444030c6e48a713c1ae32cd02a2923cc853.

--- quasar-project/src/boot/osm.js
+++
@@ -1,9 +0,0 @@
-import OpenLayersMap, {
-  Geometries,
-  Layers,
-  Map,
-  Sources,
-} from "vue3-openlayers";
-export default ({ app, router, store }) => {
-  app.use(OpenLayersMap);
-};
--- quasar-project/src/pages/points.js
+++
@@ -1,6 +0,0 @@
-import { ref } from "vue";
-const arrayWith500Points = ref([
-  [22, 52],
-  [22.3, 52.3],
-]);
-export default arrayWith500Points;
