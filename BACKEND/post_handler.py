cd63a526cd5dd00c09d7cb7028411523ca794a22
BachaBajceps <dla50@interia.pl>
2024-05-25 18:34:23 +0200

    od nowa

--- BACKEND/post_handler.py
+++ BACKEND/post_handler.py
@@ -6,34 +6,13 @@ import os
 
 client = MongoClient("mongodb://localhost:27017")
 db = client["Baza"]
-notatki = db["Notatka"]
+notatki = db["Ogloszenia"]
 
-def wyslanieWiadmosciPost(nazwa_projektu: str, nazwa_przedmiotu: str, files: List[UploadFile]):
-    for file in files:
-        # Zapisywanie pliku na serwerze
-        file_path = f"DB/{file.filename}"
-        with open(file_path, "wb") as buffer:
-            contents = file.file.read()
-            buffer.write(contents)
+def createOgloszenie():
 
-        notatka = {
-            'ID': file.filename,
-            'nazwa_projektu': nazwa_projektu,
-            'nazwa_przedmiotu': nazwa_przedmiotu,
-            'nazwa_uzytkownika': 'Uzytkownik',
-            'data': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            'content': file_path,
-            'format': file.content_type
-        }
-        result = notatki.insert_one(notatka)
-    # Zwracanie wyniku
-    if result.inserted_id:
-        return {"message": "Pliki zostały pomyślnie przesłane i zapisane w bazie danych"}
-    else:
-        return {"message": "Wystąpił błąd podczas zapisywania plików w bazie danych"}
 
-def dodajNazweProjektu(nazwa: str):
+def modifyOgloszenie():
     pass
 
-def dodajNazwePrzedmiotu(nazwa: str, nazwa_projektu: str):
+def usunOgloszenie():
     pass
