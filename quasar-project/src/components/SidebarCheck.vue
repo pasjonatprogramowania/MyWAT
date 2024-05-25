0387c67ec16f4a9b10ccd711b8ea5fb905582c72
pasjonatprogramowania <44599809+pasjonatprogramowania@users.noreply.github.com>
2024-05-25 21:12:02 +0200

    siema

---
+++ quasar-project/src/components/SidebarCheck.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="q-pa-lg">
+    <q-list bordered class="rounded-borders">
+      <q-expansion-item
+        expand-separator
+        icon="perm_identity"
+        label="Account settings"
+        caption="Filtry"
+      >
+      <q-option-group
+        v-model="group"
+        :options="options"
+        color="primary"
+        type="checkbox"
+      />
+      </q-expansion-item>
+     </q-list>
+    <q-expansion-item
+      expand-separator
+      icon="signal_wifi_off"
+      label="Dodawanie punktów"
+    >
+      <q-btn color="primary" text-color="white" label="Dodaj" />
+      <q-btn color="primary" text-color="white" label="Usuń" />
+      <q-btn color="primary" text-color="white" label="Popraw" />
+    </q-expansion-item>
+    <q-expansion-item
+      expand-separator
+      icon="signal_wifi_off"
+      label="Przejazdy"
+    >
+      <TimeInput></TimeInput>
+      <q-btn color="primary" text-color="white" label="Pokaż" />
+      </q-expansion-item>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import TimeInput from "components/TimeInput.vue";
+
+export default {
+  components: {TimeInput},
+  setup () {
+    return {
+      group: ref(['op1']),
+
+      options: [
+        {
+          label: 'Kołka zainteresowanie ',
+          value: 'kz'
+        },
+        {
+          label: 'Imprezy eventy watowe',
+          value: 'iw'
+        },
+        {
+          label: 'Ogłoszenia',
+          value: 'og'
+        },
+
+      ]
+    }
+  }
+}
+</script>
